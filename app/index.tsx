5ac8293c392a94a7445471f3ea4c3a302f0b29fb
Zainab Manzoor <zainabmanzoorzoor@gmail.com>
2026-02-03 23:47:52 +0500

    setup

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={{color: "white"}}>Hello World!</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container :{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#5f3f79"
+  }
+});
