a8923143136ae3e0b296135b8438cddc186eca62
Zainab Manzoor <zainabmanzoorzoor@gmail.com>
2026-02-04 20:47:30 +0500

    modified

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
-import { Stack } from "expo-router";
-
-export default function RootLayout() {
-  return <Stack />;
+import { Stack } from "expo-router/stack";
+export default function RootLayout(){
+  return (
+    <Stack>
+      <Stack.Screen 
+        name="index" 
+        options={{
+         headerTitle: "BudgetBite",
+       }}
+      />
+      <Stack.Screen 
+      name="about"
+      options={{
+        headerTitle:"BudgetBite",
+      }} />
+    </Stack>
+  );
 }
