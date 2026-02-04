ef2b8fbcf8a2b8945e4daf15ed7f02f30731efe1
Zainab Manzoor <zainabmanzoorzoor@gmail.com>
2026-02-04 20:48:34 +0500

    modified

---
+++ app/about.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About Screen</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25392e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+  },
+});
