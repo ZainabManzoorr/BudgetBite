384f2c69756d1a0887e1c2e88481f6cd66b628ec
Zainab Manzoor <zainabmanzoorzoor@gmail.com>
2026-02-20 23:09:50 +0500

    modified

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,12 @@
-import { Stack } from "expo-router/stack";
+import { Stack } from "expo-router";
 export default function RootLayout(){
   return (
     <Stack>
-      <Stack.Screen 
-        name="index" 
-        options={{
-          headerShown:false
-        }}
-      />
-      <Stack.Screen 
-      name="Signup"
-      options={{
-        headerTitle:"BudgetBite",
-      }} />
+      <Stack.Screen name="Splash" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="SignupScreen" options={{ headerTitle: "BudgetBite" }} />
+      <Stack.Screen name="SignInScreen" options={{ headerTitle: "BudgetBite" }} />
+      <Stack.Screen name="home" options={{ headerShown: false }} />
     </Stack>
   );
 }
